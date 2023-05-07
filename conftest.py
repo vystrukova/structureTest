e6343d844f433754af3259ed7f11cd9b38e0a12a
Valeriya.Strukova <valeriya.strukova@shire.local>
2023-05-07 22:24:17 +0300

    test

--- conftest.py
+++ conftest.py
@@ -11,11 +11,6 @@ import config.projects.google.info
 from config.projects.google.info import Info
 # from config.projects.yandex.info import Info
 
-supported_browsers = {
-    'chrome': webdriver.Chrome,
-    'firefox': webdriver.Firefox
-}
-
 supported_projects = {
     'yandex': config.projects.yandex.info.Info,
     'google': config.projects.google.info.Info
@@ -23,29 +18,8 @@ supported_projects = {
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='chrome',
-                     help="Choose browser: chrome or firefox")
     parser.addoption('--project_name', action='store', default='yandex',
                      help='Choose project:')
-
-
-@pytest.fixture(scope="session")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-
-    if browser_name == "firefox":
-        print(f"\nstart firefox browser for test..")
-        browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
-    elif browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    else:
-        joined_browsers = ', '.join(supported_browsers.keys())
-        raise pytest.UsageError(f"--browser_name is invalid, supported browsers: {joined_browsers}")
-
-    browser.maximize_window()
-    yield browser
-    browser.quit()
 
 
 @pytest.fixture(scope="session")
